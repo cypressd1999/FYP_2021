0e399b54dfb80d37419c449fdbe4cfb69e037c85
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2022-03-03 01:56:16 +0800

    Update main_task.py

--- main_task.py
+++ main_task.py
@@ -81,9 +81,10 @@ if __name__ == "__main__":
             E1 = sent[sent.find('[E1] ')+5:sent.find(' [/E1]')]
             E2 = sent[sent.find('[E2] ')+5:sent.find(' [/E2]')]
             results.append(tuple((E1, E2, result[1])))
-        print(results)    
+        print("Relations extracted:")
+        print(results)
+        print("")
             
-    
     if args.task == 'fewrel':
         fewrel = FewRel(args)
         meta_input, e1_e2_start, meta_labels, outputs = fewrel.evaluate()
